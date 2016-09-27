83bec84e9d4ae7078d540d0ecc7c6b2b8d8b256f
Anna-Zvankovich <anya.zvan@gmail.com>
2016-09-27 22:02:46 +0300

    квадратное уравнение

--- Source.cpp
+++ Source.cpp
@@ -2,108 +2,48 @@
 
 using namespace std;
 
+int main()
+{
 
-int main() {
-
-	int first, second;
-	bool check = true;
-
-	first = 2;
-	second = 8;
-
-	switch (first) {
-	case 0: break;
-	case 1:	{
-		check = false;
-		switch (second) {
-		case 0:
-			cout << "ten";
-			break;
-		case 1:
-			cout << "eleven";
-			break;
-		case 2:
-			cout << "twelve";
-			break;
-		case 3:
-			cout << "thirteen";
-			break;
-		case 4:
-			cout << "fourteen";
-			break;
-		case 5:
-			cout << "fifteen";
-			break;
-		case 6:
-			cout << "sixteen";
-			break;
-		case 7:
-			cout << "seventeen";
-			break;
-		case 8:
-			cout << "eighteen";
-			break;
-		case 9:
-			cout << "nineteen";
-		}
+	double a, b, c;
+	cout << "Enter coeff." << endl;
+	cout << "a : ";
+	cin >> a;
+	if (a == 0)
+	{
+		cout << "Input error!" << endl;
+		system("pause");
+		return 0;
 	}
-		break;
-	case 2: 
-		cout << "twenty ";
-		break;
-	case 3: 
-		cout << "thirty ";
-		break;
-	case 4: 
-		cout << "fourty ";
-		break;
-	case 5:
-		cout << "fifty ";
-		break;
-	case 6:
-		cout << "sixty ";
-		break;
-	case 7:
-		cout << "seventy ";
-		break;
-	case 8:
-		cout << "eighty ";
-		break;
-	case 9:
-		cout << "ninety ";
-	}
+	cout << "b: ";
+	cin >> b;
+	cout << "c: ";
+	cin >> c;
 
-	if (check) switch (second) {
-	case 0:
-		break;
-	case 1: 
-		cout << "one";
-		break;
-	case 2:
-		cout << "two";
-		break;
-	case 3:
-		cout << "three";
-		break;
-	case 4:
-		cout << "four";
-		break;
-	case 5:
-		cout << "five";
-		break;
-	case 6: 
-		cout << "six";
-		break;
-	case 7: 
-		cout << "seven";
-		break;
-	case 8: 
-		cout << "eight";
-		break;
-	case 9:
-		cout << "nine";
+	if (a == 1)
+		cout << "x^2";
+		
+	else if (a == -1)
+		cout << "-x^2";
+	else
+		cout << a << "*x^2";
+	if (b != 0)
+	{
+		if (b == 1)
+			cout << " + x";
+		else if (b == -1)
+			cout << " -x";
+		else if (b > 0)
+			cout << " + " << b << "*x";
+		else
+			cout << " " << b << "*x";
 	}
-	cout << endl;
-	system("pause");
+	if (c != 0)
+		if (c > 0)
+			cout << " + " << c << endl;
+		else
+			cout << " " << c << endl;
+	system(" pause");
 	return 0;
-}
+
+	}
