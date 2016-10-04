d4f2d81d4bcf87321eb49435eb9eb34f78e080cb
Anna-Zvankovich <anya.zvan@gmail.com>
2016-10-04 20:06:19 +0300

    Add files via upload

---
+++ zadanie13.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int long long a;
+	int i(0);
+	cout << "a = ";
+	cin >> a;
+	while (a > 0)
+	{
+		a /= 10;
+		i++;
+	}
+	system("cls");
+	cout << "Otvet: " << i << endl;
+	system("pause");
+	return 0;
+}
