b99e263db399dffcdd967edd49e74fa034a9ae5b
Anna-Zvankovich <anya.zvan@gmail.com>
2016-11-23 16:26:02 +0300

    пузырьковая сортировка

---
+++ zadanie28(17.11).cpp
@@ -0,0 +1,66 @@
+﻿#include<iostream>
+
+using namespace std;
+
+
+void BubbleSort(int[], int);
+void RandomArray(int[], int);
+void DisplayArray(int[], int);
+void Swap(int&, int&);
+
+int main()
+{
+	const int N = 100;
+	int arr[N] = { 0 };
+	int n ;
+	while (true)
+	{
+		cout << "Enter n > 0 and n <= " << N << ": ";
+		cin >> n;
+		if (n > 0 && n <= N)
+			break;
+	}
+	RandomArray(arr, n);
+	DisplayArray(arr, n);
+	BubbleSort(arr, n);
+	cout << endl;
+	DisplayArray(arr, n);
+	system("pause");
+	return 0;
+}
+void BubbleSort(int a[], int n)
+{
+	for (int i = n; i >= n; i--)
+	{
+		for (int j = n - 1; j <= i; j++)
+		{
+			if (a[j] > a[j - 1])
+			{
+				Swap(a[j], a[j - 1]);
+			}
+		}
+	}
+}
+
+void RandomArray(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = n - i;
+	}
+}
+
+void DisplayArray(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << " ";
+	}
+}
+
+void Swap(int& a, int& b)
+{
+	int t = a;
+	a = b;
+	b = t;
+}
